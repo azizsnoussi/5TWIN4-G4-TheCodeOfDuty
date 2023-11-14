789d8a03f4aa394af8eec6cd14d5c85aaaef745f
dorramakhloufi <dorra.makhloufi@esprit.tn>
2023-11-14 21:45:35 +0100

    test2

--- src/test/java/tn/esprit/spring/kaddem/service/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/service/DepartementServiceImplTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.services.DepartementServiceImpl;
