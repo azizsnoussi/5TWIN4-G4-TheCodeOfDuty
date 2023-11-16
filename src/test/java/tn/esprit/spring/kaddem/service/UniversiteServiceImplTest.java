c4719a5c9b6dc7ced0a77e4134a430992c08c348
Asma BOUCHELLIGA <asma.bouchelliga@esprit.tn>
2023-11-16 15:02:49 +0100

    class test universite changesss

--- src/test/java/tn/esprit/spring/kaddem/service/UniversiteServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/service/UniversiteServiceImplTest.java
@@ -2,6 +2,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
 import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
 import tn.esprit.spring.kaddem.services.UniversiteServiceImpl;
@@ -46,11 +46,13 @@ public class UniversiteServiceImplTest {
         universite.setIdUniv(1);
         universite.setNomUniv("Example University");
 
-        when(universiteRepository.findById(anyInt())).thenReturn(java.util.Optional.of(universite));
+        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
 
         Universite retrievedUniversite = universiteService.retrieveUniversite(universite.getIdUniv());
 
         assertThat(retrievedUniversite).isNotNull();
+        assertThat(retrievedUniversite.getIdUniv()).isEqualTo(1); // Validate specific attributes
+        assertThat(retrievedUniversite.getNomUniv()).isEqualTo("Example University");
     }
 
     @Test
@@ -63,9 +65,11 @@ public class UniversiteServiceImplTest {
 
         when(universiteRepository.findAll()).thenReturn(universites);
 
-        Set<Universite> retrievedUniversites = new HashSet<>(universiteService.retrieveAllUniversites());
+        Set<Universite> retrievedUniversites = universiteService.retrieveAllUniversites();
 
         assertThat(retrievedUniversites).isNotNull();
+        assertThat(retrievedUniversites).hasSize(1); // Validate size
+        assertThat(retrievedUniversites.iterator().next().getIdUniv()).isEqualTo(1); // Validate specific attributes
     }
 
     @Test
@@ -74,7 +78,7 @@ public class UniversiteServiceImplTest {
         mockUniversite.setIdUniv(1);
         mockUniversite.setNomUniv("Example University");
 
-        when(universiteRepository.findById(1)).thenReturn(java.util.Optional.of(mockUniversite));
+        when(universiteRepository.findById(1)).thenReturn(Optional.of(mockUniversite));
         doNothing().when(universiteRepository).delete(mockUniversite);
 
         universiteService.addUniversite(mockUniversite);
